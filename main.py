e5b4d8f50a16f64995e03cfac2c231c34d14bc1a
Luis Kcomt <45114652+kcomt@users.noreply.github.com>
2022-08-30 19:38:43 -0500

    random movement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
 import render1
 
-render = render1.Render1(800,800,80)
+render = render1.Render1(800,800,60)
 render.render()
--- render1.py
+++ render1.py
@@ -1,30 +1,59 @@
 from pickle import TRUE
+from turtle import width
 import pygame
+import random
+
 
 class Render1:
     def __init__(self, width, height, bpm):
         pygame.init()
+        self.width = width
+        self.height = height
+        self.gameDisplay = pygame.display.set_mode((width, height))
+        self.bpm = bpm
         self.x = 100
         self.y = 100
-        self.dx = 3
+        self.radius = width/10
+        self.dx = 2
         self.dy = 3
-        self.gameDisplay = pygame.display.set_mode((800,800))
-        pygame.display.set_caption('A bit Racey')
+        pygame.display.set_caption('kcomtpy')
         self.clock = pygame.time.Clock()
+        self.gameRunning = TRUE
+        self.randomColor = (random.randint(0, 255),   random.randint(
+            0, 255), random.randint(0, 255))
 
     def draw(self):
-        BLUE =      (  0,   0, 255)
-        pygame.draw.circle(self.gameDisplay, BLUE ,(self.x, self.y), 50)
+        pygame.draw.circle(self.gameDisplay, self.randomColor, (self.x, self.y), self.radius)
 
     def move(self):
+        moveX = random.randint(0, 100)
+        moveY = random.randint(0, 100)
+        
+        if (moveX == 99):
+            self.dx = self.dx * -1
+            
+        if (moveY == 99):
+            self.dy = self.dy * -1
+
+        if (self.x + self.dx - self.radius < 0 or self.x + self.dx + self.radius > self.width):
+            self.dx = self.dx * -1
+        
+        if (self.y + self.dy - self.radius< 0 or self.y + self.dy + self.radius> self.height):
+            self.dy = self.dy * -1
+        
         self.x = self.x + self.dx
         self.y = self.y + self.dy
 
+    def handleEvents(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.gameRunning = False
+
     def render(self):
-        while TRUE:
+        while self.gameRunning:
+            self.handleEvents()
             self.move()
             pygame.display.update()
-            self.gameDisplay.fill((0,0,0))
-            self.draw()   
-            self.clock.tick(60)
-
+            self.gameDisplay.fill((0, 0, 0))
+            self.draw()
+            self.clock.tick(self.bpm)
