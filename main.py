6a093879d88f8f84fe7d94072ab59771a7a52c3d
Luis Kcomt <45114652+kcomt@users.noreply.github.com>
2022-09-05 17:21:36 -0500

    Render 3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import render1
 import render2
+import render3
 
-render = render2.Render2(800,800,80)
+render = render3.Render3(800,800,80)
 render.render()
---
+++ render3.py
@@ -0,0 +1,94 @@
+import pygame
+import random
+
+class circle:
+    def __init__(self, width, height, bpm, gameDisplay, order):
+        self.width = width
+        self.height = height
+        self.randomColor = (255,255,255)
+        self.gameDisplay = gameDisplay
+        self.radius = width/15
+
+        if(order == 1):
+            self.x = self.radius*2
+            self.y = self.radius*2
+            self.dx = bpm/4
+            self.dy = bpm*2
+
+        elif (order == 2):
+            self.x = width-self.radius*2
+            self.y = self.radius*2
+            self.dx = bpm/4
+            self.dy = bpm*2
+
+        elif (order == 3):
+            self.x = self.radius*2
+            self.y = self.radius*2
+            self.dx = bpm*2
+            self.dy = bpm/4
+
+        elif (order == 4):
+            self.x = self.radius*2
+            self.y = height-self.radius*2
+            self.dx = bpm*2
+            self.dy = bpm/4
+
+
+    def draw(self):
+        #pygame.draw.circle(self.gameDisplay, self.randomColor, (self.x, self.y), self.radius)
+        pygame.draw.rect(self.gameDisplay, self.randomColor, (self.x, self.y, self.radius, self.radius))
+
+    def move(self):
+        if (self.x + self.dx - self.radius < 0 or self.x + self.dx + self.radius > self.width):
+            self.dx = self.dx * -1
+        
+        if (self.y + self.dy - self.radius< 0 or self.y + self.dy + self.radius> self.height):
+            self.dy = self.dy * -1
+        
+        self.x = self.x + self.dx
+        self.y = self.y + self.dy
+
+class Render3:
+    def __init__(self, width, height, bpm):
+        pygame.init()
+        pygame.display.set_caption('kcomtpy')
+        self.width = width
+        self.height = height
+        self.gameDisplay = pygame.display.set_mode((width, height))
+        self.bpm = bpm
+        self.clock = pygame.time.Clock()
+        self.gameRunning = True
+        self.createBalls()
+
+    def createBalls(self):
+        self.balls = []
+        circle1 = circle(self.width, self.height, self.bpm, self.gameDisplay, 1)
+        circle2 = circle(self.width, self.height, self.bpm, self.gameDisplay, 2)
+        circle3 = circle(self.width, self.height, self.bpm, self.gameDisplay, 3)
+        circle4 = circle(self.width, self.height, self.bpm, self.gameDisplay, 4)
+        self.balls.append(circle1)
+        self.balls.append(circle2)
+        self.balls.append(circle3)
+        self.balls.append(circle4) 
+
+    def draw(self):
+        for ball in self.balls:
+            ball.draw()
+
+    def move(self):
+        for ball in self.balls:
+            ball.move()
+
+    def handleEvents(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.gameRunning = False
+
+    def render(self):
+        while self.gameRunning:
+            self.handleEvents()
+            self.move()
+            pygame.display.update()
+            self.gameDisplay.fill((0, 0, 0))
+            self.draw()
+            self.clock.tick(self.bpm/2)
